1b5ea7fd3ab9d0d4c8361f44462e97e449224c15
Kenaz <kenaz@puppetlabs.com>
2020-06-12 21:19:53 -0700

    Update EBS steps with STS support

--- actions/steps/ebs-describe-volumes/step.py
+++ actions/steps/ebs-describe-volumes/step.py
@@ -17,10 +17,17 @@ def volume_to_dict(ec2, volume):
 
 relay = Interface()
 
+session_token = None
+try:
+  session_token = relay.get(D.aws.connection.sessionToken)
+except:
+  pass
+
 sess = boto3.Session(
   aws_access_key_id=relay.get(D.aws.connection.accessKeyID),
   aws_secret_access_key=relay.get(D.aws.connection.secretAccessKey),
   region_name=relay.get(D.aws.region),
+  aws_session_token=session_token
 )
 ec2 = sess.resource('ec2')
 raw_volumes = ec2.volumes.all()
